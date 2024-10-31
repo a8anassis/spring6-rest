d958873da1aefa757d5320114c911cb0d511816c
Th. Androutsos <a8anassis@gmail.com>
2024-10-31 19:10:57 +0200

    updates

--- src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.schoolapp.repository;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 

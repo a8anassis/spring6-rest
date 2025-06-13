aa9d8bbe70d6c69667d6354c14ac66f2ad847b96
a8anassis <a8anassis@gmail.com>
2025-06-13 19:15:17 +0300

    fix: clear

--- src/main/java/gr/aueb/cf/schoolapp/authentication/CustomUserDetailsService.java
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.schoolapp.authentication;
 
 import gr.aueb.cf.schoolapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +13,11 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+//    @Autowired
+//    public CustomUserDetailsService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username)
--- src/main/java/gr/aueb/cf/schoolapp/core/ErrorHandler.java
+++ src/main/java/gr/aueb/cf/schoolapp/core/ErrorHandler.java
@@ -3,7 +3,6 @@ package gr.aueb.cf.schoolapp.core;
 import gr.aueb.cf.schoolapp.core.exceptions.*;
 import gr.aueb.cf.schoolapp.dto.ResponseMessageDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
